5b2493799b834f3051babfe78bd31a0eb5d2ed29
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-03-09 20:22:16 +0900

    뷰 생성

--- MVC-Example/MVC-Example/View.swift
+++ MVC-Example/MVC-Example/View.swift
@@ -5,4 +5,21 @@
 //  Created by 이성호 on 2023/03/09.
 //
 
-import Foundation
+import UIKit
+
+class TodoItemCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: "cell")
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with todoItem: TodoItem) {
+        self.textLabel?.text = todoItem.title
+        self.accessoryType = todoItem.isCompleted ? .checkmark : .none
+    }
+}
