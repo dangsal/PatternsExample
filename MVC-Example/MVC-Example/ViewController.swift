d7ae351864d284696d996558cbeb4838f9b073d0
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-03-09 20:18:10 +0900

    뷰컨트롤러 테이블뷰 생성

--- MVC-Example/MVC-Example/ViewController.swift
+++ MVC-Example/MVC-Example/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "cell")
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupDelegation()
+        setLayout()
     }
 
+    private func setLayout() {
+        view.addSubview(tableView)
+        tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        tableView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        tableView.widthAnchor.constraint(equalToConstant: 500).isActive = true
+        tableView.heightAnchor.constraint(equalToConstant: 500).isActive = true
+    }
+    
+    private func setupDelegation() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {}
