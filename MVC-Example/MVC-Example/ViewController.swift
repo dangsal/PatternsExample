6d91a1aba5b42cddba845400feb9d188d9147359
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-03-09 20:30:06 +0900

    데이터 연결

--- MVC-Example/MVC-Example/ViewController.swift
+++ MVC-Example/MVC-Example/ViewController.swift
@@ -9,25 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var todoItems: [TodoItem] = []
+    
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.register(UITableViewCell.self,
+        tableView.register(TodoItemCell.self,
                            forCellReuseIdentifier: "cell")
         return tableView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupData()
         setupDelegation()
         setLayout()
     }
 
     private func setLayout() {
         view.addSubview(tableView)
-        tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         tableView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        tableView.widthAnchor.constraint(equalToConstant: 500).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         tableView.heightAnchor.constraint(equalToConstant: 500).isActive = true
     }
     
@@ -35,18 +38,47 @@ class ViewController: UIViewController {
         self.tableView.delegate = self
         self.tableView.dataSource = self
     }
+    
+    private func setupData() {
+        todoItems = [
+            TodoItem(title: "MVC 패턴 공부하기",
+                     isCompleted: false),
+            TodoItem(title: "코드리뷰하기",
+                     isCompleted: false),
+            TodoItem(title: "애니또 코드 탐색",
+                     isCompleted: true)
+        ]
+    }
+    
+    private func addTodoItem(withTitle title: String) {
+        let newTodoItem = TodoItem(title: title, isCompleted: false)
+        todoItems.append(newTodoItem)
+        tableView.reloadData()
+    }
+    
+    private func toggleTodoOtemCompletion(at index: Int) {
+        todoItems[index].isCompleted = !todoItems[index].isCompleted
+        tableView.reloadData()
+    }
 
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return todoItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TodoItemCell
+        let todoItem = todoItems[indexPath.row]
+        
+        cell.configure(with: todoItem)
         return cell
     }
 }
 
-extension ViewController: UITableViewDelegate {}
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        toggleTodoOtemCompletion(at: indexPath.row)
+    }
+}
