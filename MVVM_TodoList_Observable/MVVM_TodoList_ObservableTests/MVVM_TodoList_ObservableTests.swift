fd55c3e0222f1e03da6455ae099a5e3066845a71
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-06-01 19:43:01 +0900

    TodoList 초기세팅

---
+++ MVVM_TodoList_Observable/MVVM_TodoList_ObservableTests/MVVM_TodoList_ObservableTests.swift
@@ -0,0 +1,36 @@
+//
+//  MVVM_TodoList_ObservableTests.swift
+//  MVVM_TodoList_ObservableTests
+//
+//  Created by 이성호 on 2023/06/01.
+//
+
+import XCTest
+@testable import MVVM_TodoList_Observable
+
+final class MVVM_TodoList_ObservableTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
