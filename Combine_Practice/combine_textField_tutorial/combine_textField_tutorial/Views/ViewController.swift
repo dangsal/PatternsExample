2b341d1f4115d0a29d2b73574c9ef1a34925ca36
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-06-05 10:48:26 +0900

    초기 셋팅

---
+++ Combine_Practice/combine_textField_tutorial/combine_textField_tutorial/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  combine_textfield_tutorial
+//
+//  Created by 이성호 on 2023/06/05.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Combine_Practice/combine_textField_tutorial/combine_textField_tutorialUITests/combine_textField_tutorialUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  combine_textfield_tutorialUITestsLaunchTests.swift
+//  combine_textfield_tutorialUITests
+//
+//  Created by 이성호 on 2023/06/05.
+//
+
+import XCTest
+
+final class combine_textfield_tutorialUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
